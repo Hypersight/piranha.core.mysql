31e8b72396a68d9c2be7efa9d9bbdfbf2f51a046
aatmmr <maikm@kth.se>
2019-08-27 17:51:37 +0200

    Updated licence info header

--- src/Piranha.AspNetCore.Identity.MySQL/DbFactory.cs
+++ src/Piranha.AspNetCore.Identity.MySQL/DbFactory.cs
@@ -1,11 +1,11 @@
 ﻿#if DEBUG
 /*
- * Copyright (c) 2018 aatmmr
+ * Copyright (c) 2019 aatmmr
  *
  * This software may be modified and distributed under the terms
- * of the MIT license.  See the LICENSE file for details.
+ * of the MIT license. See the LICENSE file for details.
  * 
- * http://github.com/piranhacms/piranha.core
+ * https://github.com/piranhacms/piranha.core.mysql
  * 
  */
 
